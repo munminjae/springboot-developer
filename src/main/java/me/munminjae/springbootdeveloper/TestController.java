3a73e691b51985a6433938d66ae1443eed0b65e1
munminjae <oja5068@naver.com>
2023-11-27 13:39:41 +0900

    springboot 실행 테스트 코드 작성

--- src/main/java/me/munminjae/springbootdeveloper/TestController.java
+++ src/main/java/me/munminjae/springbootdeveloper/TestController.java
@@ -1,15 +1,22 @@
 package me.munminjae.springbootdeveloper;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 public class TestController {
+
+    @Autowired
+    TestService testService;
     @GetMapping("/test")
-    public String test() {
-         return "Hello, world";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
